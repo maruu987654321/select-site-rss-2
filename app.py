d381f6542cc37c4d4f91a646a63210763c48c28a
mariy <45437484+ruta777@users.noreply.github.com>
2019-01-05 19:46:10 +0400

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def get_rss_amazon(key_words):
     list_review = []
     list_url = [] 
     for i in key_words:
-        print(i)
+        print(amazonscraper.search(i))
            
 def get_rss_etsy(key_words):
     store = file.Storage('token.json')
